7b4c4ae3c1dc62866c9aa2c1cb1d32374d231dbd
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2024-10-16 19:17:25 -0300

    first commit

---
+++ src/main/java/org/example/practicemanagementsystem/PracticeManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package org.example.practicemanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/practicemanagementsystem/PracticeManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.practicemanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
