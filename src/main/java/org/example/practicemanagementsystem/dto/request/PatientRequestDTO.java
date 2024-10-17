179839ada2ff5dfa0d58b3541e9906e3aa15cd0c
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2024-10-17 18:55:28 -0300

    feat: remove annotation

--- src/main/java/org/example/practicemanagementsystem/dto/request/PatientRequestDTO.java
+++ src/main/java/org/example/practicemanagementsystem/dto/request/PatientRequestDTO.java
@@ -1,10 +1,9 @@
 package org.example.practicemanagementsystem.dto.request;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
-@AllArgsConstructor
+
 public class PatientRequestDTO {
     private Long id;
     private String name;
