f747ef40a4bfebbee7b0853d211932e15a307afe
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2024-10-17 18:54:39 -0300

    feat: add phone and adress in request

--- src/main/java/org/example/practicemanagementsystem/dto/request/PatientRequestDTO.java
+++ src/main/java/org/example/practicemanagementsystem/dto/request/PatientRequestDTO.java
@@ -1,10 +1,14 @@
 package org.example.practicemanagementsystem.dto.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class PatientRequestDTO {
     private Long id;
     private String name;
     private String email;
+    private String phone;
+    private String address;
 }
