7da95d669ab428e4cf56576b319c9fa5b27efc06
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2024-10-19 18:27:06 -0300

    fix: add optional in methods findByName in repositories

--- src/main/java/org/example/practicemanagementsystem/repository/DoctorRepository.java
+++ src/main/java/org/example/practicemanagementsystem/repository/DoctorRepository.java
@@ -4,7 +4,9 @@ import org.example.practicemanagementsystem.model.DoctorModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface DoctorRepository extends JpaRepository<DoctorModel, Long> {
-    DoctorModel findByName(String name);
+    Optional<DoctorModel> findByName(String name);
 }
