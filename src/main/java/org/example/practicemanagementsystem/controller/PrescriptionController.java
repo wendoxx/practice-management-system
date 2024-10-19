1845f4e9f6d0ea4e79ed2a8c7399d79e8e770e95
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2024-10-19 16:11:06 -0300

    feat: add prescription controller

---
+++ src/main/java/org/example/practicemanagementsystem/controller/PrescriptionController.java
@@ -0,0 +1,30 @@
+package org.example.practicemanagementsystem.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/prescriptions")
+public class PrescriptionController {
+
+    @Autowired
+    private PrescriptionService prescriptionService;
+
+    @GetMapping
+    public String findById(){
+        return "find by id";
+    }
+
+    @GetMapping
+    public String findByName(){
+        return "find by name";
+    }
+
+    @GetMapping("/all")
+    public String findAll(){
+        return "find all";
+    }
+
+}
