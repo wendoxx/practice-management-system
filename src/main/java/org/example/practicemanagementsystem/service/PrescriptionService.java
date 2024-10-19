56e1ddfa3dd09b917cad019151414106dbe19de1
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2024-10-19 18:29:04 -0300

    feat: add PrescriptionService and method to save prescription

---
+++ src/main/java/org/example/practicemanagementsystem/service/PrescriptionService.java
@@ -0,0 +1,69 @@
+package org.example.practicemanagementsystem.service;
+
+import lombok.AllArgsConstructor;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.example.practicemanagementsystem.dto.request.PrescriptionRequestDTO;
+import org.example.practicemanagementsystem.model.DoctorModel;
+import org.example.practicemanagementsystem.model.PatientModel;
+import org.example.practicemanagementsystem.model.PrescriptionModel;
+import org.example.practicemanagementsystem.repository.DoctorRepository;
+import org.example.practicemanagementsystem.repository.PatientRepository;
+import org.example.practicemanagementsystem.repository.PrescriptionRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
+public class PrescriptionService {
+
+    @Autowired
+    PrescriptionRepository prescriptionRepository;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    private static final Logger LOGGER = LogManager.getLogger(PrescriptionService.class);
+
+    @Autowired
+    private DoctorRepository doctorRepository;
+
+    @Autowired
+    PatientRepository patientRepository;
+
+
+    public PrescriptionModel createPrescription(PrescriptionRequestDTO prescriptionRequestDTO) {
+        PrescriptionModel prescription;
+
+        if (prescriptionRequestDTO.getId() != null && prescriptionRepository.existsById(prescriptionRequestDTO.getId())) {
+            LOGGER.info("Updating prescription");
+            prescription = prescriptionRepository.findById(prescriptionRequestDTO.getId()).get();
+            LOGGER.info("Prescription updated!");
+        } else {
+            LOGGER.info("Saving new prescription...");
+            prescription = new PrescriptionModel();
+            LOGGER.info("Prescription saved!");
+        }
+
+        DoctorModel doctor = (doctorRepository.findById(prescriptionRequestDTO.getId())).orElseThrow(() -> {
+            LOGGER.error("Doctor not found.");
+            return new RuntimeException("Doctor not found.");
+        });
+
+        PatientModel patient = (patientRepository.findById(prescriptionRequestDTO.getId())).orElseThrow(() -> {
+            LOGGER.error("Patient not found");
+            return new RuntimeException("Patient not found.");
+        });
+
+        prescription.setPatient(patient);
+        prescription.setDoctor(doctor);
+        prescription.setContent(prescriptionRequestDTO.getContent());
+
+        return prescriptionRepository.save(prescription);
+    }
+
+
+}
