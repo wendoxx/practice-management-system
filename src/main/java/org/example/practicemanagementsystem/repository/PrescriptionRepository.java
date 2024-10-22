04a9d201f927a1ba7136fb61fe252de22c879494
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2024-10-21 23:18:37 -0300

    feat: add findAllByDoctor in PrescriptionRepository

--- src/main/java/org/example/practicemanagementsystem/repository/PrescriptionRepository.java
+++ src/main/java/org/example/practicemanagementsystem/repository/PrescriptionRepository.java
@@ -1,9 +1,15 @@
 package org.example.practicemanagementsystem.repository;
 
+import org.example.practicemanagementsystem.model.DoctorModel;
+import org.example.practicemanagementsystem.model.PatientModel;
 import org.example.practicemanagementsystem.model.PrescriptionModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PrescriptionRepository extends JpaRepository<PrescriptionModel, Long> {
+    Optional<PrescriptionModel> findByPatient(PatientModel patient);
+    Optional<PrescriptionModel> findByDoctor(DoctorModel doctor);
 }
