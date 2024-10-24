572ec916ab2037a128c74d36047366efac9d1f4a
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2024-10-23 21:41:44 -0300

    fix: change attributes type

--- src/main/java/org/example/practicemanagementsystem/dto/request/PrescriptionRequestDTO.java
+++ src/main/java/org/example/practicemanagementsystem/dto/request/PrescriptionRequestDTO.java
@@ -1,15 +1,12 @@
 package org.example.practicemanagementsystem.dto.request;
 
 import lombok.Data;
-import org.example.practicemanagementsystem.dto.response.AppointmentResponseDTO;
-import org.example.practicemanagementsystem.dto.response.DoctorResponseDTO;
-import org.example.practicemanagementsystem.dto.response.PatientResponseDTO;
 
 @Data
 public class PrescriptionRequestDTO {
     private Long id;
-    private PatientResponseDTO patient;
-    private DoctorResponseDTO doctor;
-    private AppointmentResponseDTO appointment;
+    private Long doctor;
+    private Long patient;
+    private Long appointment;
     private String content;
 }
