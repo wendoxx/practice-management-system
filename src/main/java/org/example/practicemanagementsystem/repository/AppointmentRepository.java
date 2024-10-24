22ddd17f148537874b5804eab6435912a7ffd890
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2024-10-24 17:58:18 -0300

    feat: add methods to find by patient and by doctor in AppointmentRepository

--- src/main/java/org/example/practicemanagementsystem/repository/AppointmentRepository.java
+++ src/main/java/org/example/practicemanagementsystem/repository/AppointmentRepository.java
@@ -1,10 +1,15 @@
 package org.example.practicemanagementsystem.repository;
 
 import org.example.practicemanagementsystem.model.AppointmentModel;
+import org.example.practicemanagementsystem.model.DoctorModel;
+import org.example.practicemanagementsystem.model.PatientModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface AppointmentRepository extends JpaRepository<AppointmentModel, Long> {
-
+    Optional<AppointmentModel> findAllByDoctor(DoctorModel doctorModel);
+    Optional<AppointmentModel> findAllByPatient(PatientModel patientModel);
 }
