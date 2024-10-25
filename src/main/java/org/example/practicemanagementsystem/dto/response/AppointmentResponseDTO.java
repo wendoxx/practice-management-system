a9bc5294c55cfac533329dd93fd9e85e6cb94b14
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2024-10-24 23:46:27 -0300

    fix: change attribute types in AppointmentResponseDTO

--- src/main/java/org/example/practicemanagementsystem/dto/response/AppointmentResponseDTO.java
+++ src/main/java/org/example/practicemanagementsystem/dto/response/AppointmentResponseDTO.java
@@ -2,11 +2,13 @@ package org.example.practicemanagementsystem.dto.response;
 
 import lombok.Data;
 
+import java.time.LocalDateTime;
+
 @Data
 public class AppointmentResponseDTO {
     private Long id;
-    private String doctor;
-    private String patient;
-    private String dateAndHour;
+    private DoctorResponseDTO doctor;
+    private PatientResponseDTO patient;
+    private LocalDateTime date;
     private String description;
 }
