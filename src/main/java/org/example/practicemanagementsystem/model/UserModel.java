ba3a55f793a35d9bf75cbe636d24789a7b42c2c6
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2024-10-28 20:29:57 -0300

    feat: add authorities

--- src/main/java/org/example/practicemanagementsystem/model/UserModel.java
+++ src/main/java/org/example/practicemanagementsystem/model/UserModel.java
@@ -1,9 +1,13 @@
 package org.example.practicemanagementsystem.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import org.example.practicemanagementsystem.role.UserRole;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
@@ -14,6 +18,8 @@ import java.util.UUID;
 @Table(name = "tb_users")
 @Data
 @EqualsAndHashCode(of = "id")
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserModel implements UserDetails {
 
     @Id
@@ -26,39 +32,54 @@ public class UserModel implements UserDetails {
     @Column(name = "password")
     private String password;
 
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of();
+    @Column
+    private UserRole role;
+
+    public UserModel (String username, String password, UserRole role) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
     }
 
     @Override
-    public String getPassword() {
-        return "";
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        if (this.role == UserRole.ADMIN){
+            return List.of(new SimpleGrantedAuthority("ROLE_ADMIN"), new SimpleGrantedAuthority("USER_ROLE"), new SimpleGrantedAuthority("DOCTOR_ROLE"));
+        } else if (this.role == UserRole.DOCTOR) {
+            return List.of(new SimpleGrantedAuthority("DOCTOR_ROLE"), new SimpleGrantedAuthority("USER_ROLE"));
+        } else {
+            return List.of(new SimpleGrantedAuthority("USER_ROLE"));
+        }
     }
 
     @Override
     public String getUsername() {
-        return "";
+        return username;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
     }
 
     @Override
     public boolean isAccountNonExpired() {
-        return UserDetails.super.isAccountNonExpired();
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return UserDetails.super.isAccountNonLocked();
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return UserDetails.super.isCredentialsNonExpired();
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        return UserDetails.super.isEnabled();
+        return true;
     }
 
 }
