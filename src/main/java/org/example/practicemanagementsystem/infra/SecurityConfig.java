8a74d67028bce38c5a50e5441adf275829703f71
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2024-10-30 00:46:22 -0300

    feat: add new filter

--- src/main/java/org/example/practicemanagementsystem/infra/SecurityConfig.java
+++ src/main/java/org/example/practicemanagementsystem/infra/SecurityConfig.java
@@ -1,16 +1,26 @@
 package org.example.practicemanagementsystem.infra;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Autowired
+    SecurityFilter securityFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
@@ -23,8 +33,10 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.DELETE,"/api/v1/patient/delete-patient/{id}").hasRole("ADMIN")
                         .requestMatchers(HttpMethod.GET,"/api/v1/patient/all").hasRole("ADMIN")
 
-                        //login
+                        //login and register
                         .requestMatchers(HttpMethod.POST,"/api/v1/login").permitAll()
+                        .requestMatchers(HttpMethod.POST,"/api/v1/register").permitAll()
+
 
                         //doctor
                         .requestMatchers(HttpMethod.POST, "/api/v1/doctor").hasRole("ADMIN")
@@ -50,6 +62,17 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.DELETE, "/api/v1/prescription/delete-prescription/{id}").hasRole("ADMIN")
 
                         .anyRequest().authenticated()
-                ) .build();
+                )
+                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
